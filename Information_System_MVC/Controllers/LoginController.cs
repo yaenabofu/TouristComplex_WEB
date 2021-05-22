898b9e463fb9695dfb828f1ca2b0272fdf0927fb
yaenabofu <nezzterlivejuke@gmail.com>
2021-05-23 03:35:46 +0500

    № 11 - Login web page

---
+++ Information_System_MVC/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Information_System_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Enter()
+        {
+            return View();
+        }
+    }
+}
